f8798b9894d6d242f6623c3a7f3cf9e2dd42dbd1
David Li <madheadsproductions@gmail.com>
2018-02-02 16:11:04 -0500

    Add files via upload

---
+++ scrapers/top100Scrape.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import urllib
+import time
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+url=urllib.urlopen('http://store.steampowered.com/stats/').read()
+steampage = BeautifulSoup(url,'lxml')
+
+timestamp = time.time()
+currentTime = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+top100CSV = open('SteamTop100byTime.csv', 'w')
+top100CSV.write('time,id,name,current,max\n')
+for row in steampage('tr', {'class': 'player_count_row'}):
+    steamAppID = row.a.get('href').split("/")[4]
+    steamGameName = row.a.get_text().encode('utf-8')
+    currentConcurrent = row.find_all('span')[0].get_text()
+    maxConcurrent = row.find_all('span')[1].get_text()
+    
+    top100CSV.write('{0},{1},"{2}","{3}","{4}"\n'.format(currentTime, steamAppID, steamGameName, currentConcurrent, maxConcurrent))
+
+top100CSV.close()
+
+steamOverallCSV = open('SteamOverallbyTime.csv', 'w')
+
+for row in steampage('div', {'class': 'statsTop'}):
+    steamOverallCurrentConcurrent = row.find_all('span')[0].get_text()
+    steamOverallMaxConcurrent = row.find_all('span')[1].get_text()
+    
+    steamOverallCSV.write('{0},"{1}","{2}"\n'.format(currentTime, steamOverallCurrentConcurrent, steamOverallMaxConcurrent))
+    
+steamOverallCSV.close()
